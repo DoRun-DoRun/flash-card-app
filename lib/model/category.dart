f3c6595c209b1aa01979baa4f1b298aeb662582c
hin6150 <hin6150@gmail.com>
2024-04-23 12:25:15 +0900

    feat: 모델 생성

---
+++ lib/model/category.dart
@@ -0,0 +1,9 @@
+enum CategoryCode {
+  animal("animal", "동물"),
+  plant("plant", "식물"),
+  color("clolr", "색깔");
+
+  final String korean;
+  final String english;
+  const CategoryCode(this.korean, this.english);
+}
