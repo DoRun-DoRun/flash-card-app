db9e986b474e01fd618120e5de9ead286540c9e9
hin6150 <hin6150@gmail.com>
2024-06-04 13:25:24 +0900

    feat: 데이터 추가

--- lib/model/category.dart
+++ lib/model/category.dart
@@ -4,13 +4,14 @@ enum CategoryCode {
   animal("동물", "animal", Icons.pets_outlined),
   plant("식물", "plant", Icons.local_florist_outlined),
   food("음식", "food", Icons.fastfood_outlined),
-  color("색깔", "color", Icons.color_lens_outlined),
   occupation("직업", "occupation", Icons.work_outline),
   nature("자연", "nature", Icons.forest_outlined),
   transportation("교통", "transportation", Icons.directions_car_outlined),
   family("가족", "family", Icons.family_restroom_outlined),
   activity("활동", "activity", Icons.surfing_outlined),
-  culture("문화", "culture", Icons.theater_comedy_outlined);
+  culture("문화", "culture", Icons.theater_comedy_outlined),
+  electronics("전자제품", "electronics", Icons.devices_outlined),
+  emotion("감정", "emotion", Icons.emoji_emotions_outlined);
 
   final String korean;
   final String english;
